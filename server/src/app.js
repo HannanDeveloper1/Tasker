ad352bcc93d3da8bd5dc8dea5c8406a8e9840ee8
Hannan Dev <hannandeveloper1@gmail.com>
2025-06-19 11:44:35 +0500

    Handle 404 error

--- server/src/app.js
+++ server/src/app.js
@@ -32,6 +32,13 @@ app.get(
   })
 );
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    message: "Requested resource was not found",
+  });
+});
+
 app.use(ErrorMiddleware);
 
 export default app;
