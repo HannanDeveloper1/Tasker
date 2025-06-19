ba1ca5fa51a84ffc0133e8f071c3de8591be359a
Hannan Dev <hannandeveloper1@gmail.com>
2025-06-19 11:38:49 +0500

    Handle Server Exceptions

--- server/index.js
+++ server/index.js
@@ -7,3 +7,50 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.on("error", (err) => {
+  console.log(err);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received, graceful shutdown");
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received, graceful shutdown");
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+});
+
+process.on("SIGKILL", () => {
+  console.log("SIGKILL received, graceful shutdown");
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+});
+
+process.on("SIGHUP", () => {
+  console.log("SIGHUP received, graceful shutdown");
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+});
+
+process.on("exit", () => {
+  console.log("exit received, graceful shutdown");
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+});
